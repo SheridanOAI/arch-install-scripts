22d905c297d5aa757b568b5afbf79805e5d10524
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2021-10-10 12:29:20 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -2,11 +2,20 @@
 
 PACKAGES="f2fs-tools dosfstools ntfs-3g p7zip unrar ark aspell-ru firefox firefox-i18n-ru yakuake packagekit-qt5 gwenview flameshot kate dolphin audacious pavucontrol-qt gnome-disk-utility gnome-mahjongg conky ttf-liberation ttf-dejavu"
 
-TOOLS="grub os-prober efibootmgr plasma nvidia-settings"
+    echo '(П.21 стр.35) Выбор установки nvidia drivers c kernel ($NV_DEFAULT,$NV_ZEN,$NV_LTS),'
+NV_DEFAULT="nvidia nvidia-settings"
+NV_ZEN="nvidia-dkms nvidia-settings"
+NV_LTS="nvidia-lts nvidia-settings"
 
-NVIDIA_DEFAULT="nvidia"
-NVIDIA_ZEN="nvidia-dkms"
-NVIDIA_LTS="nvidia-lts"
+    echo '(П.21 стр.35) Установка AMD_ATI drivers'
+AMD_ATI="xorg-server xorg-drivers"
+
+    echo '(П.22 стр.37) Выбор установки рабочего стола ($DE_PLASMA,$DE_CINNAMON,$DE_GNOME,$DE_XFCE,$DE_MATE'
+DE_PLASMA="plasma"
+DE_CINNAMON="cinnamon"
+DE_GNOME="gnome gnome-extra"
+DE_XFCE="xfce4 xfce4-goodies"
+DE_MATE="mate mate-extra"
 
     echo '15. Выставляем регион'
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
@@ -17,30 +26,33 @@ sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
     echo '18. Указываем язык системы'
 echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
-    echo '19. Русифицыруем консоль'
+    echo '19. Русифицируем консоль'
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
     echo '20. Обновляем базу PACMAN'
 pacman -Sy
-    echo '21. Устанавливаем DE Plasma NVIDIA GRUB'
-pacman -S $NVIDIA_DEFAULT $TOOLS
-    echo '22. Создаем root пароль'
+    echo '21. Устанавливаем NVIDIA AMD_ATI drivers'
+pacman -S $NVIDIA_DEFAULT
+    echo '22. Устанавливаем рабочий стол (DE)'
+pacman -S $DE_PLASMA
+    echo '23. Создаем root пароль'
 passwd
-    echo '23. Создаём пользователя'
+    echo '24. Создаём пользователя'
 useradd -m -G users,wheel,audio,video -s /bin/bash sheridan
-    echo '24. Устанавливаем пароль пользователя'
+    echo '25. Устанавливаем пароль пользователя'
 passwd sheridan
-    echo '25. Раскоментируем sudoers'
+    echo '26. Раскоментируем sudoers'
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
-    echo '26. Включаем daemon SDDM'
+    echo '27. Подключаем daemon SDDM'
 systemctl enable sddm
-    echo '27. Включаем daemon NetworkManager'
+    echo '28. Подключаем daemon NetworkManager'
 systemctl enable NetworkManager
-    echo '28 Устанавливаем grub'
+    echo '29 Устанавливаем grub'
+pacman -S grub os-prober efibootmgr
 grub-install /dev/sda
-    echo '29. Подключение os-prober'
+    echo '30. Подключение os-prober'
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-    echo '30. Обновляем grub'
+    echo '31. Обновляем grub'
 grub-mkconfig -o /boot/grub/grub.cfg
-    echo '31. Устанавливаем программы'
+    echo '32. Устанавливаем программы'
 pacman -S $PACKAGES
