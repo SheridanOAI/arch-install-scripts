020e9b8c028e01b3dafae5464f64774fa2f580ed
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2021-12-17 12:43:16 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -14,40 +14,36 @@ setfont cyr-sun16
   DATA_LOCATION=/mnt/data
  DATA2_LOCATION=/mnt/data2
 
-    echo 'Выбор разделов'
- BOOT_PARTITION=/dev/sdxx
- SWAP_PARTITION=/dev/sdxx
- ROOT_PARTITION=/dev/sdxx
- DATA_PARTITION=/dev/sdxx
-DATA2_PARTITION=/dev/sdxx
-
     echo 'Выбор FS ROOT раздела'
         FS_TYPE=ext4
 
-    echo 'Имя компьютера'
-       HOSTNAME=archlinux
-
     echo '01. Форматирование корневого раздела'
-mkfs.$FS_TYPE $ROOT_PARTITION -L Arch
+read -p 'ROOT_PARTITION_' ROOT_PARTITION_
+mkfs.$FS_TYPE $ROOT_PARTITION_ -L Arch
     echo '02. Монтирование корневого раздела'
-mount $ROOT_PARTITION $ROOT_LOCATION
+read -p 'ROOT_PARTITION_' ROOT_PARTITION_
+mount $ROOT_PARTITION_ $ROOT_LOCATION
     echo '03. Создание папок для разделов с данными'
 mkdir /mnt/{data,data2}
     echo '04. Создание папок /boot/efi'
 mkdir -p /mnt/boot/efi
     echo '05. Монтирование загрузочного UEFI раздела'
-mount $BOOT_PARTITION $BOOT_LOCATION
+read -p 'BOOT_PARTITION_' BOOT_PARTITION_
+mount $BOOT_PARTITION_ $BOOT_LOCATION
     echo '06. Монтирование раздела с данными 1'
-mount $DATA_PARTITION $DATA_LOCATION
+read -p 'DATA_PARTITION_' DATA_PARTITION_
+mount $DATA_PARTITION_ $DATA_LOCATION
     echo '07. Монтирование раздела с данными 2'
-mount $DATA2_PARTITION $DATA2_LOCATION
-    echo 'Монтирование раздела SWAP'
-swapon $SWAP_PARTITION
-    echo '08. Копирование скрипта arch2.sh'
+read -p 'DATA2_PARTITION_' DATA2_PARTITION_
+mount $DATA2_PARTITION_ $DATA2_LOCATION
+    echo '08. Монтирование раздела SWAP'
+read -p 'SWAP_PARTITION_' SWAP_PARTITION_
+swapon $SWAP_PARTITION_
+    echo '09. Копирование скрипта arch2.sh'
 cp /1/arch2.sh /mnt/arch2.sh
-    echo '09. Установка зеркал'
+    echo '10. Установка зеркал'
 #pacman -Sy reflector && reflector --verbose -l 5 -p sort rate --save /etc/pacman.d/mirrorlist
-    echo '10. Установка ядра и основных пакетов'
+    echo '11. Установка ядра и основных пакетов'
     echo '1 - DEFAULT, 2 - ZEN, 3 - LTS'
     read choice
       if [[ "$choice" == "1" ]]; then
@@ -58,12 +54,13 @@ KERNEL=$ZEN
 KERNEL=$LTS
       fi
 pacstrap /mnt $KERNEL
-    echo '11. Генерируем fstab'
+    echo '12. Генерируем fstab'
 genfstab -U /mnt >> /mnt/etc/fstab
-    echo '12. Имя компьютера'
-echo "$HOSTNAME" >> /mnt/etc/hostname
-    echo '13. Добавляем multilib'
+    echo '13. Имя компьютера'
+read -p 'HOSTNAME_' HOSTNAME_
+echo "$HOSTNAME_" >> /mnt/etc/hostname
+    echo '14. Добавляем multilib'
 echo "[multilib]" >> /mnt/etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist/" >> /mnt/etc/pacman.conf
-    echo '14. Переход в новое окружение'
+    echo '15. Переход в новое окружение'
 arch-chroot /mnt /bin/bash /arch2.sh
