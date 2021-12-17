dd490d82eb7d38b05f9f3d9f2f8d3725052ca4e4
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2021-12-17 12:44:25 +0300

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -15,35 +15,27 @@ NV_DEFAULT="nvidia nvidia-settings"
       XFCE="xfce4 xfce4-goodies networkmanager lxdm pulseaudio picom pavucontrol"
       MATE="mate mate-extra network-manager-applet networkmanager picom mate-media lxdm pulseaudio pavucontrol"
 
-    echo 'Вводим имя пользователя'
-  USERNAME=user
-
-    echo 'Вводим такое же имя пользователя для пароля'
-  USERPASS=user
-
     echo '(П.27) Выбор экранного менеджера SDDM GDM LXDM'
      SDDM=sddm
       GDM=gdm
      LXDM=lxdm
 
-    echo 'Место (ДИСК) установки GRUB'
-     DISK=/dev/sdx
-
-    echo '15. Выставляем регион'
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-    echo '16. Раскоментируем локаль системы'
+    echo '16. Выставляем регион'
+    read -p 'ZONEINFO_' ZONEINFO_
+ln -sf /usr/share/zoneinfo/$ZONEINFO_ /etc/localtime
+    echo '17. Раскоментируем локаль системы'
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
-    echo '17. Генерируем локаль системы'
+    echo '18. Генерируем локаль системы'
 locale-gen
-    echo '18. Указываем язык системы'
+    echo '19. Указываем язык системы'
 echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
-    echo '19. Русифицируем консоль'
+    echo '20. Русифицируем консоль'
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
-    echo '20. Обновляем базу PACMAN'
+    echo '21. Обновляем базу PACMAN'
 pacman -Sy
-    echo '21. Устанавливаем NVIDIA AMD_ATI drivers'
+    echo '22. Устанавливаем NVIDIA AMD_ATI drivers'
     echo '1 - NV_DEFAULT, 2 - NV_ZEN, 3 - NV_LTS, 4 - AMD_ATI'
     read choice
       if [[ "$choice" == "1" ]]; then
@@ -56,7 +48,7 @@ DRIVERS=$NV_LTS
 DRIVERS=$AMD_ATI
       fi
 pacman -S $DRIVERS
-    echo '22. Устанавливаем рабочий стол (DE)'
+    echo '23. Устанавливаем рабочий стол (DE)'
     echo '1 - PLASMA, 2 - CINNAMON, 3 - GNOME, 4 - XFCE, 5 - MATE'
     read choice
       if [[ "$choice" == "1" ]]; then
@@ -71,15 +63,18 @@ DE=$XFCE
 DE=$MATE
       fi
 pacman -S $DE
-    echo '23. Создаем root пароль'
+    echo '24. Создаем root пароль'
 passwd
-    echo '24. Создаём пользователя'
-useradd -m -G users,wheel,audio,video -s /bin/bash $USERNAME
-    echo '25. Устанавливаем пароль пользователя'
-passwd $USERPASS
-    echo '26. Раскоментируем sudoers'
+    echo '25. Создаём пользователя'
+read -p 'USERNAME_' USERNAME_
+useradd -m -G users,wheel,audio,video -s /bin/bash $USERNAME_
+    echo '26. Вписываем такое же имя пользователя'
+read -p 'USERNAME_' USERNAME_
+    echo '27. Создаём пароль пользователя'
+passwd $USERNAME_
+    echo '28. Раскоментируем sudoers'
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
-    echo '27. Подключаем daemon SDDM, GDM, LXDM'
+    echo '29. Подключаем daemon SDDM, GDM, LXDM'
     echo '1 - PLASMA - SDDM, 2 - GNOME - GDM, 3 - CINNAMON-XFCE-MATE - LXDM'
     read choice
       if [[ "$choice" == "1" ]]; then
@@ -90,14 +85,16 @@ DM=$GDM
 DM=$LXDM
       fi
 systemctl enable $DM
-    echo '28. Подключаем daemon NetworkManager'
+    echo '30. Подключаем daemon NetworkManager'
 systemctl enable NetworkManager
-    echo '29 Устанавливаем grub'
+    echo '31. Устанавливаем grub'
 pacman -S grub os-prober efibootmgr
-grub-install $DISK
-    echo '30. Подключение os-prober'
-echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-    echo '31. Обновляем grub'
+    echo '32. Выбор диска установки grub'
+read -p 'DISK_' DISK_
+grub-install $DISK_
+    echo '33. Подключение os-prober'
+#echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+    echo '34. Обновляем grub'
 grub-mkconfig -o /boot/grub/grub.cfg
-    echo '32. Устанавливаем программы'
+    echo '35. Устанавливаем программы'
 pacman -S $PACKAGES
