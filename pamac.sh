dac03e0d36287c73204a4737277bbaf7696d2401
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2022-01-18 00:10:47 +0300

    Install pamac-aur

---
+++ pamac.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+sudo pacman -S git go
+git clone https://aur.archlinux.org/yay.git
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/''yay''.tar.gz
+cd yay
+makepkg
+sudo pacman -U yay-*.zst
+yay -S pamac-aur
+yay -S archlinux-appstream-data-pamac
