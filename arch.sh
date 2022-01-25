eaf543740199df9d5ef11eec90a8af4dcaf1624a
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2022-01-25 17:29:42 +0300

    Update arch.sh
    FS BTRFS EXT4

--- arch.sh
+++ arch.sh
@@ -1,49 +1,60 @@
- #!/bin/bash
+  #!/bin/bash
 
 loadkeys ru
 setfont cyr-sun16
 
-    echo '(П.10) Выбор ядра и основных пакетов'
+    #echo '(П.10) Выбор ядра и основных пакетов'
         DEFAULT="base base-devel linux linux-firmware nano netctl dhcpcd"
             ZEN="base base-devel linux-zen linux-zen-headers linux-firmware nano netctl dhcpcd"
             LTS="base base-devel linux-lts linux-lts-headers linux-firmware nano netctl dhcpcd"
 
-    echo 'Выбор места установки разделов (LOCATION)'
+    #echo 'Выбор места установки разделов (LOCATION)'
   ROOT_LOCATION=/mnt
   BOOT_LOCATION=/mnt/boot/efi
   DATA_LOCATION=/mnt/data
  DATA2_LOCATION=/mnt/data2
 
-    echo 'Выбор FS ROOT раздела'
-        FS_TYPE=ext4
 
-    echo '01. Форматирование корневого раздела'
-read -p 'ROOT_PARTITION_' ROOT_PARTITION_
-mkfs.$FS_TYPE $ROOT_PARTITION_ -L Arch
-    echo '02. Монтирование корневого раздела'
-read -p 'ROOT_PARTITION_' ROOT_PARTITION_
-mount $ROOT_PARTITION_ $ROOT_LOCATION
-    echo '03. Создание папок для разделов с данными'
-mkdir /mnt/{data,data2}
-    echo '04. Создание папок /boot/efi'
-mkdir -p /mnt/boot/efi
-    echo '05. Монтирование загрузочного UEFI раздела'
+
+    echo '01. Выбор раздела ROOT (/dev/xxx)'
+    read -p 'DEV_' DEV_
+
+    echo '02. Форматирование раздела ROOT'
+
+    echo '1 - BTRFS, 2 - EXT4'
+    read choice
+      if [[ "$choice" == "1" ]]; then
+mkfs.btrfs -L Arch -f $DEV_ && mount $DEV_ /mnt && cd /mnt && btrfs sub cre @ && btrfs sub cre @home && btrfs sub cre @cache && btrfs sub cre @log && cd / && umount /mnt
+    elif [[ "$choice" == "2" ]]; then
+mkfs.ext4 -L Arch $DEV_ && mount $DEV_ /mnt && mkdir /mnt/{data,data2} && mkdir -p /mnt/boot/efi && cd / && umount /mnt
+      fi
+
+    echo '03. Монтирование раздела ROOT'
+        echo '1 - BTRFS, 2 - EXT4'
+    read choice
+      if [[ "$choice" == "1" ]]; then
+mount -o noatime,autodefrag,compress=zstd,subvol=@ $DEV_ /mnt && mkdir /mnt/{home,data,data2} && mkdir -p /mnt/boot/efi && mkdir -p /mnt/var/log && mkdir -p /mnt/var/cache && mount -o noatime,autodefrag,compress=zstd,subvol=@home $DEV_ /mnt/home && mount -o noatime,autodefrag,compress=zstd,subvol=@cache $DEV_ /mnt/var/cache && mount -o noatime,autodefrag,compress=zstd,subvol=@log $DEV_ /mnt/var/log
+    elif [[ "$choice" == "2" ]]; then
+mount $DEV_ /mnt
+      fi
+
+    echo '04. Монтирование раздела UEFI'
 read -p 'BOOT_PARTITION_' BOOT_PARTITION_
 mount $BOOT_PARTITION_ $BOOT_LOCATION
-    echo '06. Монтирование раздела с данными 1'
+    echo '05. Монтирование раздела с данными 1'
 read -p 'DATA_PARTITION_' DATA_PARTITION_
 mount $DATA_PARTITION_ $DATA_LOCATION
-    echo '07. Монтирование раздела с данными 2'
+    echo '06. Монтирование раздела с данными 2'
 read -p 'DATA2_PARTITION_' DATA2_PARTITION_
 mount $DATA2_PARTITION_ $DATA2_LOCATION
-    echo '08. Монтирование раздела SWAP'
+    echo '07. Монтирование раздела SWAP'
 read -p 'SWAP_PARTITION_' SWAP_PARTITION_
 swapon $SWAP_PARTITION_
-    echo '09. Копирование скрипта arch2.sh'
+    echo '08. Копирование скрипта arch2.sh'
 cp /1/arch2.sh /mnt/arch2.sh
-    echo '10. Установка зеркал'
+    echo '09. Установка зеркал'
 #pacman -Sy reflector && reflector --verbose -l 5 -p sort rate --save /etc/pacman.d/mirrorlist
-    echo '11. Установка ядра и основных пакетов'
+    echo '10. Установка ядра и основных пакетов'
     echo '1 - DEFAULT, 2 - ZEN, 3 - LTS'
     read choice
       if [[ "$choice" == "1" ]]; then
@@ -54,13 +65,13 @@ KERNEL=$ZEN
 KERNEL=$LTS
       fi
 pacstrap /mnt $KERNEL
-    echo '12. Генерируем fstab'
+    echo '11. Генерируем fstab'
 genfstab -U /mnt >> /mnt/etc/fstab
-    echo '13. Имя компьютера'
+    echo '12. Имя компьютера'
 read -p 'HOSTNAME_' HOSTNAME_
 echo "$HOSTNAME_" >> /mnt/etc/hostname
-    echo '14. Добавляем multilib'
+    echo '13. Добавляем multilib'
 echo "[multilib]" >> /mnt/etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist/" >> /mnt/etc/pacman.conf
-    echo '15. Переход в новое окружение'
+    echo '14. Переход в новое окружение'
 arch-chroot /mnt /bin/bash /arch2.sh
