b50278dea2d2c206284f8b17a614257cd596c87b
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2022-05-20 20:38:50 +0300

    Update pamac-aur.sh

--- pamac-aur.sh
+++ pamac-aur.sh
@@ -7,8 +7,23 @@ curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/''yay''.tar.gz
 cd yay
 makepkg
 sudo pacman -U yay-*.zst
-yay -S pamac-aur
-yay -S archlinux-appstream-data-pamac
+yay -S pamac-aur archlinux-appstream-data-pamac inxi
 cd ..
 rm -rf yay
 rm yay.tar.gz
+#Install printer Konika-Minolta-PgePro-1300w
+sudo pacman -U /data/Temp/Linux/ArchLinux/Printer/printer-driver-min12xxw-0.0.9-1-x86_64.pkg.tar.zst
+sudo pacman -S cups cups-pdf print-manager system-config-printer
+systemctl enable cups.service
+systemctl start cups.service
+#Install PipeWire
+sudo pacman -Sy pipewire-alsa pipewire-pulse gst-plugin-pipewire neofetch glxinfo
+#color console
+sudo sh -c 'sed -i '33cColor' /etc/pacman.conf'
+sudo cp /data/Temp/Linux/ArchLinux/Scripts/.bashrc ~/.bashrc
+echo "PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] '" | sudo tee -a /root/.bashrc
+cp -r -p /data/Temp/Linux/ArchLinux/bin ~/bin
+cp /etc/nanorc ~/.nanorc
+sudo pacman -S nano-syntax-highlighting
+echo 'include "/usr/share/nano-syntax-highlighting/*.nanorc"' > ~/.nanorc
+sudo sh -c 'echo include \"/usr/share/nano-syntax-highlighting/*.nanorc\" >> /etc/nanorc'
