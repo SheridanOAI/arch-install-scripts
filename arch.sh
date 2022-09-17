728b7dff5a56e1eb6fc3e18d60381e74a02c121c
SheridanOAI <92163869+SheridanOAI@users.noreply.github.com>
2022-09-17 18:44:40 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,79 +1,100 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 loadkeys ru
 setfont cyr-sun16
 pacman -Sy
 pacman -S unzip wget archlinux-keyring
 
-    #echo '(П.10) Выбор ядра и основных пакетов'
-        DEFAULT="base base-devel linux linux-firmware nano netctl dhcpcd amd-ucode"
-            ZEN="base base-devel linux-zen linux-zen-headers linux-firmware nano netctl dhcpcd amd-ucode"
-            LTS="base base-devel linux-lts linux-lts-headers linux-firmware nano netctl dhcpcd amd-ucode"
+#echo '(П.10) Выбор ядра и основных пакетов'
+export DEFAULT="base base-devel linux linux-firmware nano netctl dhcpcd amd-ucode"
+export ZEN="base base-devel linux-zen linux-zen-headers linux-firmware nano netctl dhcpcd amd-ucode"
+export LTS="base base-devel linux-lts linux-lts-headers linux-firmware nano netctl dhcpcd amd-ucode"
 
-    #echo 'Выбор места установки разделов (LOCATION)'
-  ROOT_LOCATION=/mnt
-  BOOT_LOCATION=/mnt/boot/efi
-  DATA_LOCATION=/mnt/data
- DATA2_LOCATION=/mnt/data2
+#echo 'Выбор места установки разделов (LOCATION)'
+export ROOT_LOCATION=/mnt
+export BOOT_LOCATION=/mnt/boot/efi
+export DATA_LOCATION=/mnt/data
+export DATA2_LOCATION=/mnt/data2
 
+echo '01. Выбор раздела ROOT (/dev/xxx)'
+read -p 'DEV_' DEV_
 
+echo '02. Форматирование раздела ROOT'
+echo '1 - BTRFS, 2 - EXT4'
+read choice
 
-    echo '01. Выбор раздела ROOT (/dev/xxx)'
-    read -p 'DEV_' DEV_
+if [[ "$choice" == "1" ]]; then
+    mkfs.btrfs -L Arch -f $DEV_ && mount $DEV_ /mnt && \
+    cd /mnt && btrfs sub cre @ && btrfs sub cre @home && \
+    btrfs sub cre @cache && btrfs sub cre @log && cd / && umount /mnt
+elif [[ "$choice" == "2" ]]; then
+    mkfs.ext4 -L Arch $DEV_ && mount $DEV_ /mnt && \
+    mkdir /mnt/{data,data2} && mkdir -p /mnt/boot/efi && \
+    cd / && umount /mnt
+fi
 
-    echo '02. Форматирование раздела ROOT'
+echo '03. Монтирование раздела ROOT'
+echo '1 - BTRFS, 2 - EXT4'
+read choice
 
-    echo '1 - BTRFS, 2 - EXT4'
-    read choice
-      if [[ "$choice" == "1" ]]; then
-mkfs.btrfs -L Arch -f $DEV_ && mount $DEV_ /mnt && cd /mnt && btrfs sub cre @ && btrfs sub cre @home && btrfs sub cre @cache && btrfs sub cre @log && cd / && umount /mnt
-    elif [[ "$choice" == "2" ]]; then
-mkfs.ext4 -L Arch $DEV_ && mount $DEV_ /mnt && mkdir /mnt/{data,data2} && mkdir -p /mnt/boot/efi && cd / && umount /mnt
-      fi
+if [[ "$choice" == "1" ]]; then
+    mount -o noatime,autodefrag,compress=zstd,subvol=@ $DEV_ /mnt && \
+    mkdir /mnt/{home,data,data2} && mkdir -p /mnt/boot/efi && \
+    mkdir -p /mnt/var/log && mkdir -p /mnt/var/cache && \
+    mount -o noatime,autodefrag,compress=zstd,subvol=@home $DEV_ /mnt/home && \
+    mount -o noatime,autodefrag,compress=zstd,subvol=@cache $DEV_ /mnt/var/cache && \
+    mount -o noatime,autodefrag,compress=zstd,subvol=@log $DEV_ /mnt/var/log
+elif [[ "$choice" == "2" ]]; then
+    mount $DEV_ /mnt
+fi
 
-    echo '03. Монтирование раздела ROOT'
-        echo '1 - BTRFS, 2 - EXT4'
-    read choice
-      if [[ "$choice" == "1" ]]; then
-mount -o noatime,autodefrag,compress=zstd,subvol=@ $DEV_ /mnt && mkdir /mnt/{home,data,data2} && mkdir -p /mnt/boot/efi && mkdir -p /mnt/var/log && mkdir -p /mnt/var/cache && mount -o noatime,autodefrag,compress=zstd,subvol=@home $DEV_ /mnt/home && mount -o noatime,autodefrag,compress=zstd,subvol=@cache $DEV_ /mnt/var/cache && mount -o noatime,autodefrag,compress=zstd,subvol=@log $DEV_ /mnt/var/log
-    elif [[ "$choice" == "2" ]]; then
-mount $DEV_ /mnt
-      fi
-
-    echo '04. Монтирование раздела UEFI'
+echo '04. Монтирование раздела UEFI'
 read -p 'BOOT_PARTITION_' BOOT_PARTITION_
 mount $BOOT_PARTITION_ $BOOT_LOCATION
-    echo '05. Монтирование раздела с данными 1'
+
+echo '05. Монтирование раздела с данными 1'
 read -p 'DATA_PARTITION_' DATA_PARTITION_
 mount $DATA_PARTITION_ $DATA_LOCATION
-    echo '06. Монтирование раздела с данными 2'
+
+echo '06. Монтирование раздела с данными 2'
 read -p 'DATA2_PARTITION_' DATA2_PARTITION_
 mount $DATA2_PARTITION_ $DATA2_LOCATION
-    echo '07. Монтирование раздела SWAP'
+
+echo '07. Монтирование раздела SWAP'
 read -p 'SWAP_PARTITION_' SWAP_PARTITION_
 swapon $SWAP_PARTITION_
-    echo '08. Копирование и распаковка архива с github'
+
+echo '08. Копирование и распаковка архива с github'
 wget https://github.com/SheridanOAI/arch-install-scripts/archive/refs/heads/main.zip
 unzip main.zip -d /mnt
-    echo '09. Установка зеркал'
+
+echo '09. Установка зеркал'
 #pacman -Sy reflector && reflector --verbose -l 5 -p sort rate --save /etc/pacman.d/mirrorlist
-    echo '10. Установка ядра и основных пакетов'
-    echo '1 - DEFAULT, 2 - ZEN, 3 - LTS'
-    read choice
-      if [[ "$choice" == "1" ]]; then
-KERNEL=$DEFAULT
-    elif [[ "$choice" == "2" ]]; then
-KERNEL=$ZEN
-    elif [[ "$choice" == "3" ]]; then
-KERNEL=$LTS
-      fi
+
+echo '10. Установка ядра и основных пакетов'
+echo '1 - DEFAULT, 2 - ZEN, 3 - LTS'
+read choice
+
+if [[ "$choice" == "1" ]]; then
+    KERNEL=$DEFAULT
+elif [[ "$choice" == "2" ]]; then
+    KERNEL=$ZEN
+elif [[ "$choice" == "3" ]]; then
+    KERNEL=$LTS
+fi
+
 pacstrap /mnt $KERNEL
-    echo '11. Генерируем fstab'
+
+echo '11. Генерируем fstab'
 genfstab -U /mnt >> /mnt/etc/fstab
-    echo '12. Имя компьютера'
+
+echo '12. Имя компьютера'
 read -p 'HOSTNAME_' HOSTNAME_
+
 echo "$HOSTNAME_" >> /mnt/etc/hostname
-    echo '13. Добавляем multilib'
+
+echo '13. Добавляем multilib'
 sed -i '93c[multilib]' /mnt/etc/pacman.conf ; sed -i '94cInclude = /etc/pacman.d/mirrorlist' /mnt/etc/pacman.conf
-    echo '14. Переход в новое окружение'
+
+echo '14. Переход в новое окружение'
 arch-chroot /mnt /bin/bash /arch-install-scripts-main/arch2.sh
